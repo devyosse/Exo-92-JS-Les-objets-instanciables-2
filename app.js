f2992b842e2c3df7ade989409f02ab92ca798721
devyosse <narutoshippuden884@gmail.com>
2021-12-03 13:52:08 +0100

    exo  92

---
+++ app.js
@@ -0,0 +1,28 @@
+let Personne = function(nom, prenom, age, sexe) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+    this.sexe = sexe;
+
+    this.getPersonne = function (){
+        return "Ma personne s'appelle " + this.nom + this.prenom + " elle a " + this.age + " et est de sexe " + this.sexe;
+    }
+}
+// creation personnnages
+let tableau = [];
+
+let Hocine = new Personne('Hassaini ', ' Hocine', '20', 'Homme');
+let Arnold = new Personne('Schwarzenegger ', ' Arnold', '45', 'Homme');
+let Ridan = new Personne('Hassaini ', 'Ridan', '10', 'Homme');
+let Elise = new Personne('Auger ', 'Elise', '20', 'Femme');
+let John = new Personne('Buchet ', 'Jonathan', '30', 'Homme');
+let Oli = new Personne('Peuchet ', 'Olivier', '24', 'Homme');
+let Kev = new Personne('Mura ', 'Kévin', '28', 'Homme');
+
+tableau.push(Hocine, Arnold, Ridan, Elise, John, Oli, Kev);
+
+for(let i = 0; i < tableau.length;i++){
+   let div = document.createElement('div');
+    div.innerHTML = tableau[i].getPersonne();
+    document.body.appendChild(div);
+}
